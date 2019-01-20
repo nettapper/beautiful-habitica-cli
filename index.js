04dcb5ce8cb7550d734219b5e9f530161b2713a9
Conner D <connerdunn7399@gmail.com>
2019-01-20 13:59:13 -0700

    init commit of output with colors, plus auto gen gitignore

---
+++ index.js
@@ -0,0 +1,5 @@
+var figlet = require('figlet');
+var chalk = require('chalk');
+
+console.log(chalk.redBright(figlet.textSync("Beautiful")));
+console.log(chalk.cyanBright(figlet.textSync("Habitica")));

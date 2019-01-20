1615ee9aeaa8c2c55ab3977ab01b482b4a59cf58
Conner D <connerdunn7399@gmail.com>
2019-01-20 14:35:07 -0700

    Got a simple datastore working for the future API Token

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 var figlet = require('figlet');
 var chalk = require('chalk');
+const Store = require('data-store')
+const store = new Store('beautiful-habitica-cli');
+const API_TOKEN = 'habitica-api-token';
+// console.log(store.set(API_TOKEN, "hello world"));
+console.log(store.get(API_TOKEN));
+console.log(typeof(store.get('test'))); // undefined if not present
 
-console.log(chalk.redBright(figlet.textSync("Beautiful")));
-console.log(chalk.cyanBright(figlet.textSync("Habitica")));
+console.log(chalk.redBright(figlet.textSync('Beautiful')));
+console.log(chalk.cyanBright(figlet.textSync('Habitica')));
